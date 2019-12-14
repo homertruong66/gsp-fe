a1671cd40bebb703ec5f671dfa9df15a335fd0a4
SangCao1995 <caobathanhsang@gmail.com>
2019-12-14 10:06:31 +0700

    Search Admins

--- jquery_app/controllers/SecurityController.js
+++ jquery_app/controllers/SecurityController.js
@@ -1,6 +1,7 @@
 const SecurityController = {
     login: function(users) {
         if(email === users[0].email && password === users[0].password) {
+            localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ0b3B0YWwuY29tIiwiZXhwIjoxNDI2NDIwODAwLCJodHRwOi8vdG9wdGFsLmNvbS9qd3RfY2xhaW1zL2lzX2FkbWluIjp0cnVlLCJjb21wYW55IjoiVG9wdGFsIiwiYXdlc29tZSI6dHJ1ZX0.yRQYnWzskCZUxPwaQupWkiUzKELZ49eM7oWxAQK_ZXw');
             $('#login').removeClass('active');
             $('#page').addClass('active');
         }
@@ -12,6 +13,9 @@ const SecurityController = {
         }
     },
     logout: function() {
+        const token = localStorage.getItem('token');
+        // TODO: POST id và token lên API
+        localStorage.clear();
         $('#page').removeClass('active');
         $('#login').addClass('active');
         $('#box')[0].reset();
